92f885940eddf5dad640144b1db63be45538336f
Igor Eskin <eskinhd@gmail.com>
2022-07-22 17:22:33 -0400

    Dependency array

--- src/PostDetail.js
+++ src/PostDetail.js
@@ -1,7 +1,11 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
+  console.log("POST ID: ", postId);
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
   );
+  console.log(response);
   return response.json();
 }
 
@@ -23,7 +27,24 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id),
+    { staleTime: 2000 }
+  );
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Error</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
